acf7e78f9414001c4deecb2f4213c4610b3fe317
williambl <william@williambl.com>
2025-06-03 00:20:58 +0100

    feat: fisp expression parsing + printing

--- src/main/java/com/williambl/buskymore/PostFilter.java
+++ src/main/java/com/williambl/buskymore/PostFilter.java
@@ -1,10 +1,9 @@
 package com.williambl.buskymore;
 
-import java.text.NumberFormat;
 import java.util.*;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 @FunctionalInterface
 public interface PostFilter extends Predicate<Post> {
@@ -14,42 +13,209 @@ public interface PostFilter extends Predicate<Post> {
 
     // fisp (filter lisp)
     sealed interface Fisp {
-        static Fisp parse(String value) {
-            return new Str("unimplemented"); //todo
+        static Fisp.Array parse(String input) {
+            int[] cursor = new int[] {0};
+            return parseList(input, cursor);
+        }
+
+        private static Fisp.Array parseList(String input, int[] cursor) {
+            List<Fisp> contents = new ArrayList<>();
+            while (cursor[0] < input.length()) {
+                var charAt = input.charAt(cursor[0]);
+                if (Character.isWhitespace(charAt)) {
+                    cursor[0]++;
+                    continue;
+                } else if (charAt == '(') {
+                    cursor[0]++;
+                    contents.add(parseList(input, cursor));
+                } else if (charAt == ')') {
+                    cursor[0]++;
+                    break;
+                } else {
+                    contents.add(parseAtom(input, cursor));
+                }
+            }
+            return new Array(List.copyOf(contents));
+        }
+
+        private static Fisp parseAtom(String input, int[] cursor) {
+            StringBuilder res = new StringBuilder(input.length());
+            boolean isQuoted = input.charAt(cursor[0]) == '"';
+            if (isQuoted) {
+                cursor[0]++;
+            }
+            boolean isEscaped = false;
+            while (cursor[0] < input.length()) {
+                var charAt = input.charAt(cursor[0]);
+                if (!isQuoted) {
+                    if (Character.isWhitespace(charAt)
+                            || charAt == '('
+                            || charAt == ')'
+                            || charAt == '"') {
+                        break;
+                    }
+                } else {
+                    if (isEscaped) {
+                        if (charAt == 'u') {
+                            int codepoint = Integer.parseInt(input.substring(cursor[0]+1, cursor[0]+5), 16);
+                            res.appendCodePoint(codepoint);
+                            cursor[0] += 4;
+                            continue;
+                        } else if (charAt == 'b') {
+                            charAt = '\b';
+                        } else if (charAt == 'f') {
+                            charAt = '\f';
+                        } else if (charAt == 'n') {
+                            charAt = '\n';
+                        } else if (charAt == 'r') {
+                            charAt = '\r';
+                        } else if (charAt == 't') {
+                            charAt = '\t';
+                        }
+                    } else {
+                        if (charAt == '"') {
+                            cursor[0]++;
+                            break;
+                        }
+                        if (charAt == '\\') {
+                            isEscaped = true;
+                        }
+                    }
+                }
+                res.append(charAt);
+                cursor[0]++;
+            }
+
+            String value = res.toString();
+            if (!isQuoted) {
+                if (value.equals("true")) {
+                    return new Bool(true);
+                } else if (value.equals("false")) {
+                    return new Bool(false);
+                }
+            }
+
+            return new Str(value);
+        }
+
+        static String print(Fisp expression) {
+            var sb = new StringBuilder();
+            if (expression instanceof Array) {
+                printInternal(expression, sb, true);
+            } else {
+                printInternal(new Array(List.of(expression)), sb, true);
+            }
+            return sb.toString();
+        }
+
+        private static void printInternal(Fisp value, StringBuilder sb, boolean isRoot) {
+            switch (value) {
+                case Array(List<Fisp> elements) -> {
+                    if (elements.isEmpty()) {
+                        if (!isRoot) {
+                            sb.append("()");
+                        }
+                        return;
+                    }
+
+                    if (elements.size() == 1) {
+                        if (isRoot) {
+                            sb.append("(");
+                        }
+                        printInternal(elements.getFirst(), sb, false);
+                        if (isRoot) {
+                            sb.append(")");
+                        }
+                        return;
+                    }
+
+                    int sizeBeforeList = sb.length();
+                    boolean shouldUseNewlines = false;
+                    int[] whitespaces = new int[elements.size() + 1];
+                    if (!isRoot) {
+                        sb.append("( ");
+                    }
+                    whitespaces[0] = sb.length() - 1;
+                    for (int i = 0; i < elements.size(); i++) {
+                        var element = elements.get(i);
+                        int sizeBeforeElement = sb.length();
+                        printInternal(element, sb, false);
+                        sb.append(' ');
+                        whitespaces[i + 1] = sb.length() - 1;
+                        if (IntStream.range(sizeBeforeElement, sb.length()).map(sb::charAt).anyMatch(c -> c == '\n')) {
+                            shouldUseNewlines = true;
+                        }
+                    }
+                    if (!isRoot) {
+                        sb.append(")");
+                    }
+                    if (sb.length() - sizeBeforeList > 80) {
+                        shouldUseNewlines = true;
+                    }
+                    shouldUseNewlines = false; //todo need to be able to parse multiline expressions haha
+                    //noinspection ConstantValue
+                    if (shouldUseNewlines) {
+                        for (int idx : whitespaces) {
+                            sb.setCharAt(idx, '\n');
+                        }
+                    }
+                }
+                case Bool(boolean bool) -> {
+                    sb.append(bool);
+                }
+                case Str(String str) -> {
+                    if (str.equals("true") || str.equals("false")
+                            || str.codePoints().anyMatch(c ->
+                            c == '"'
+                                    || c == '\\'
+                                    || c == '\b'
+                                    || c == '\f'
+                                    || c == '\n'
+                                    || c == '\r'
+                                    || c == '\t'
+                                    || Character.isISOControl(c) || !Character.isDefined(c))
+                    ) {
+                        sb.append('"');
+                        str.codePoints().forEachOrdered(codepoint -> {
+                            if (codepoint == '\"') {
+                                sb.append("\"");
+                            } else if (codepoint == '\\') {
+                                sb.append("\\\\");
+                            } else if (codepoint == '\b') {
+                                sb.append("\\b");
+                            } else if (codepoint == '\f') {
+                                sb.append("\\f");
+                            } else if (codepoint == '\n') {
+                                sb.append("\\n");
+                            } else if (codepoint == '\r') {
+                                sb.append("\\r");
+                            } else if (codepoint == '\t') {
+                                sb.append("\\t");
+                            } else if (Character.isISOControl(codepoint) || !Character.isDefined(codepoint)) {
+                                sb.append("\\u").append("%04x".formatted(codepoint).toUpperCase(Locale.ROOT));
+                            } else {
+                                sb.appendCodePoint(codepoint);
+                            }
+                        });
+                        sb.append('"');
+                    } else {
+                        sb.append(str);
+                    }
+                }
+            }
         }
 
         record Bool(boolean value) implements Fisp {
             @Override
             public String toString() {
-                return Boolean.toString(this.value);
+                return print(this);
             }
         }
+
         record Str(String value) implements Fisp {
             @Override
             public String toString() {
-                StringBuilder res = new StringBuilder();
-                this.value.codePoints().forEachOrdered(codepoint -> {
-                    if (codepoint == '\"') {
-                        res.append("\"");
-                    } else if (codepoint == '\\') {
-                        res.append("\\\\");
-                    } else if (codepoint == '\b') {
-                        res.append("\\b");
-                    } else if (codepoint == '\f') {
-                        res.append("\\f");
-                    } else if (codepoint == '\n') {
-                        res.append("\\n");
-                    } else if (codepoint == '\r') {
-                        res.append("\\r");
-                    } else if (codepoint == '\t') {
-                        res.append("\\t");
-                    } else if (Character.isISOControl(codepoint) || !Character.isDefined(codepoint)) {
-                        res.append("\\u").append("%04x".formatted(codepoint).toUpperCase(Locale.ROOT));
-                    } else {
-                        res.appendCodePoint(codepoint);
-                    }
-                });
-                return res.toString();
+                return print(this);
             }
         }
         record Array(List<Fisp> values) implements Fisp {
@@ -63,40 +229,7 @@ public interface PostFilter extends Predicate<Post> {
 
             @Override
             public String toString() {
-                if (this.values.isEmpty()) {
-                    return "()";
-                }
-
-                if (this.values.size() == 1) {
-                    return "(" + this.values.getFirst().toString() + ")";
-                }
-
-                StringBuilder result = new StringBuilder();
-                boolean shouldUseNewlines = false;
-                int[] whitespaces = new int[this.values.size() + 1];
-                result.append("( ");
-                whitespaces[0] = result.length() - 1;
-                List<Fisp> fisps = this.values;
-                for (int i = 0; i < fisps.size(); i++) {
-                    var value = fisps.get(i);
-                    String valueAsString = value.toString();
-                    result.append(valueAsString);
-                    result.append(' ');
-                    whitespaces[i + 1] = result.length() - 1;
-                    if (valueAsString.contains("\n")) {
-                        shouldUseNewlines = true;
-                    }
-                }
-                result.append(")");
-                if (result.length() > 80) {
-                    shouldUseNewlines = true;
-                }
-                if (shouldUseNewlines) {
-                    for (int idx : whitespaces) {
-                        result.setCharAt(idx, '\n');
-                    }
-                }
-                return result.toString();
+                return print(this);
             }
         }
     }
@@ -149,11 +282,11 @@ public interface PostFilter extends Predicate<Post> {
     }
 
     static void bootstrap() {
-        PARSER.register("and", (fisp, parser) -> {
+        PARSER.register("all_of", (fisp, parser) -> {
             var args = parser.parse(fisp.arguments());
             return post -> args.stream().allMatch(p -> p.test(post));
         });
-        PARSER.register("or", (fisp, parser) -> {
+        PARSER.register("any_of", (fisp, parser) -> {
             var args = parser.parse(fisp.arguments());
             return post -> args.stream().anyMatch(p -> p.test(post));
         });
